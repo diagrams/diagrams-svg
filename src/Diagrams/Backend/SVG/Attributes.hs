97d2bca8559cc2c7184f7af0a47814c4d1ba2c20
Jeffrey Rosenbluth <jeffrey.rosenbluth@gmail.com>
2015-06-25 15:33:02 -0400

    minor edits to SVG.Attributes

--- src/Diagrams/Backend/SVG/Attributes.hs
+++ src/Diagrams/Backend/SVG/Attributes.hs
@@ -1,34 +1,64 @@
-{-| Attributes that are specific to the SVG backend. The intent
-    of this module is to allow adding attributes like class,
-    id, and data attributes to an SVG. For those embedding 
-    the resulting SVG into a webpage, this allows some 
-    interactivity with javascript and stylesheets.
--}
-
 {-# LANGUAGE DeriveDataTypeable #-}
 
-module Diagrams.Backend.SVG.Attributes where
+-----------------------------------------------------------------------------
+-- |
+-- Module      :  Diagrams.Backend.SVG.Attributes
+-- Copyright   :  (c) 2015 Diagrams team (see LICENSE)
+-- License     :  BSD-style (see LICENSE)
+-- Maintainer  :  diagrams-discuss@googlegroups.com
+--
+-- Attributes that are specific to the SVG backend. The intent
+-- of this module is to allow adding the attributes class,
+-- and id attributes to an SVG. For those embedding
+-- the resulting SVG into a webpage, this allows some
+-- interactivity with javascript and stylesheets.
+--
+-----------------------------------------------------------------------------
 
-import Data.Semigroup
-import Diagrams.Core.Style (AttributeClass,HasStyle,applyAttr)
-import Data.Typeable (Typeable)
+module Diagrams.Backend.SVG.Attributes (
+  -- * Id
+    SvgId(..)
+  , svgId
 
-data SvgId = SvgId {getSvgId :: String}
-  deriving (Typeable)
-instance Semigroup SvgId where 
+  -- * Class
+  , SvgClass(..)
+  , svgClass
+
+  ) where
+
+import         Diagrams.Core.Style (AttributeClass, HasStyle, applyAttr)
+import         Data.Semigroup
+import         Data.Typeable       (Typeable)
+
+-----------------------------------------------------------------
+--  Id
+-----------------------------------------------------------------
+
+-- | The SVG id attribute.
+newtype SvgId = SvgId {getSvgId :: String}
+  deriving Typeable
+
+instance Semigroup SvgId where
   _ <> a = a
 instance AttributeClass SvgId
 
+-- | Set the Id attribute.
 svgId :: HasStyle a => String -> a -> a
-svgId val a = applyAttr (SvgId val) a
+svgId = applyAttr . SvgId
 
+-----------------------------------------------------------------
+--  Class
+-----------------------------------------------------------------
 
-data SvgClass = SvgClass {getSvgClass :: String}
-  deriving (Typeable)
-instance Semigroup SvgClass where 
+-- | The SVG class attribute.
+newtype SvgClass = SvgClass {getSvgClass :: String}
+  deriving Typeable
+
+instance Semigroup SvgClass where
   _ <> a = a
+
 instance AttributeClass SvgClass
 
+-- | Set the class attribute.
 svgClass :: HasStyle a => String -> a -> a
-svgClass val a = applyAttr (SvgClass val) a
-
+svgClass = applyAttr . SvgClass
