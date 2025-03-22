241b41ae61508bd18c4512b67868be7929438b8d
Brent Yorgey <byorgey@gmail.com>
2025-03-22 10:44:17 -0500

    remove no longer supported `DiagramLoopOpts`

--- src/Diagrams/Backend/SVG/CmdLine.hs
+++ src/Diagrams/Backend/SVG/CmdLine.hs
@@ -157,10 +157,8 @@ instance Parseable PrettyOpt where
   parser = prettyOpt
 
 instance SVGFloat n => Mainable (QDiagram SVG V2 n Any) where
-    type MainOpts (QDiagram SVG V2 n Any) = (DiagramOpts, DiagramLoopOpts, PrettyOpt)
-    mainRender (opts, loopOpts, pretty) d = do
-        chooseRender opts pretty d
-        defaultLoopRender loopOpts
+    type MainOpts (QDiagram SVG V2 n Any) = (DiagramOpts, PrettyOpt)
+    mainRender (opts, pretty) d = chooseRender opts pretty d
 
 chooseRender :: SVGFloat n => DiagramOpts -> PrettyOpt -> QDiagram SVG V2 n Any -> IO ()
 chooseRender opts pretty d =
